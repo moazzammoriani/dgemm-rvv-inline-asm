eb552c37da2d8238d7103d0047860170fb106d3c
Moazzam Moriani <mozmoriani@gmail.com>
2022-11-18 17:10:56 +0500

    Complete refactor

--- Gemm_4x4Kernel_RVV_inline.c
+++ Gemm_4x4Kernel_RVV_inline.c
@@ -5,9 +5,6 @@
 #define MR 4
 #define NR 4
 
-#include<stdio.h>
-#include<riscv_vector.h>
-
 
 void Gemm_MRxNRKernel_Packed( int k,
 		        double *MP_A, double *MP_B, double *C, int ldC )
@@ -25,8 +22,8 @@ void Gemm_MRxNRKernel_Packed( int k,
 
                 "mv            t0, zero        \n\t"
                 "add           t0, t0,2        \n\t"
-                "vsetvli       t0, t0, e64     \n\t"
-                "fmv.w.x    ft11, zero         \n\t"
+                "vsetvli       t0, t0,    e64  \n\t"
+                "fmv.d.x    ft11, zero         \n\t"
                 "mv         t0,   %[BK]        \n\t"
 
                 "vfmv.v.f   v16,  ft11         \n\t" // v16[i] = ft11              // Column 0 of microkernel C
@@ -41,50 +38,50 @@ void Gemm_MRxNRKernel_Packed( int k,
                 "vfmv.v.f   v28,  ft11         \n\t" // v28[i] = ft11              // Column 3 of microkernel C
                 "vfmv.v.f   v29,  ft11         \n\t" // v29[i] = ft11
 
-                ".align 4                      \n\t"
-                "M4x4_TAILLOOP:                \n\t"
-                
+
                 "addi       t4,    %[PA], 2*8  \n\t" // t4 = %[PA] + (2*8)  
+                "addi       t5,    %[PA], 4*8  \n\t" // t5 = %[PA] + (4*8)
+                "addi       t6,    %[PA], 6*8  \n\t" // t6 = %[PA] + (6*8)
                 "addi       t1,    %[PB], 1*8  \n\t" // t1 = %[PB] + (1*8)
                 "addi       t2,    %[PB], 2*8  \n\t" // t2 = %[PB] + (2*8)
                 "addi       t3,    %[PB], 3*8  \n\t" // t3 = %[PB] + (3*8)
-                                                     //
-                "fld        ft0,  (%[PB])      \n\t" // ft0 = (%[PB])            // ft0 = B[0, 0]
-                "addi       %[PB], %[PB], 4*8  \n\t" // %[PB] = %[PB] + (4*8)    // B[1, 0]
-                "vle64.v      v0,   (%[PA])    \n\t" // v0[0 : 2] = (%[PA])      // v0[0 : 2] = A[0 : 2, 0]
-                "add        %[PA], %[PA], 8*8  \n\t" // %[PA] += 8*8             // A[0 , 1]
-                "vle64.v      v1,   (t4)       \n\t" // v1[0 : 2] = (t4)         // v1[0 : 2] = A[2 : 4, 0]
-                "addi       t4,    t4,    8*8  \n\t" // t4 = t4 + (8*8)          // A[2, 1] 
 
-                "vfmv.v.f   v8,   ft0          \n\t" // v8[i] = ft0              // v8[i] = B[0, 0]
-                "fld        ft1,  (t1)         \n\t" // ft1 = (t1)               // ft1 = B[0, 1]
-                "addi       t1,   t1,     4*8  \n\t" // t1 = t1 + (4*8)          // B[1, 1]
+                "LOOP:                \n\t"
+                "fld        ft0,  (%[PB])      \n\t" /* ft0 = B[0, 0]             */
+                "addi       %[PB], %[PB], 4*8  \n\t" /* %[PB] = (B[1, 0])         */
+                "vfmv.v.f   v8,   ft0          \n\t" /* v8[i] = B[0, 0]           */
+                "vle64.v    v0,   (%[PA])      \n\t" /* v0[0 : 2] = A[0 : 2, 0]   */
+                "add        %[PA], %[PA], 4*8  \n\t" /* %[PA] = (A[0, 1])         */
+                "vle64.v    v1,   (t4)         \n\t" /* v1[0 : 2] = A[2 : 4, 0]   */
+                "addi       t4,    t4,    4*8  \n\t" /* t4 = (A[2, 1))            */
+                "vfmacc.vv     v16, v8, v0     \n\t" /* B[0, 0] * A[0 : 2, 0]     */
+                "vfmacc.vv     v17, v8, v1     \n\t" /* B[0, 0] * A[2 : 4, 0]     */
 
-                "vfmacc.vv  v16,  v8,    v0    \n\t" // v16[i] = (v8[i] * v0[i]) + v16[i]    // B[0, 0] * A[0 : 2, 0] + v16[i] 
-                "fld        ft2,  (t2)         \n\t" // ft2 = (t2)                           // ft2 = B[0, 2]
-                "addi       t2,   t2,     4*8  \n\t" // t2 = t2 + (4*8)                      // B[1, 2]
-                "vfmacc.vv  v17,  v8,    v1    \n\t" // v17[i] = (v8[i] * v1[i]) + v17[i]    // B[0, 0] * A[2 : 4, 0] + v17[i]
-                "vfmv.v.f   v9,   ft1          \n\t" // v9[i] = ft1                          // v9[i] = B[0, 1]
+                "fld        ft1,  (t1)         \n\t" /* ft1 = B[0, 1]             */
+                "addi       t1, t1, 4*8        \n\t" /* t1 = (B[1, 1])            */
+                "vfmv.v.f   v8,   ft1          \n\t" /* v8[i] = B[0, 1] */
+                "vfmacc.vv     v20, v8, v0     \n\t" /* B[0, 1] * A[0 : 2, 0]     */
+                "vfmacc.vv     v21, v8, v1     \n\t" /* B[0, 1] * A[2 : 4, 0]     */
 
-                "vfmacc.vv  v20,  v9,    v0    \n\t" // v20[i] = (v9[i] * v0[i]) + v20[i]    // B[0, 1] * A[0 : 2, 0] + v20[i]  
-                "fld        ft3,  (t3)         \n\t" // ft3 = (t3)                           // B[0, 3]
-                "addi       t3,   t3,     4*8  \n\t" // t3 = t3 + (4*8)
-                "vfmacc.vv  v21,  v9,    v1    \n\t" // v21[i] = (v9[i] * v1[i]) + v21[i]    // B[0, 1] * A[2 : 4, 0] + v21[i]
-                "vfmv.v.f   v10,  ft2          \n\t" // v10[i] = ft2                         // v10[i] = B[0, 2]
+                "fld        ft2,  (t2)         \n\t" /* ft2 = B[0, 2]             */
+                "addi       t2, t2, 4*8        \n\t" /* t2 = (B[1, 2])            */
+                "vfmv.v.f   v8,   ft2          \n\t" /* v8[i] = B[0, 2] */
+                "vfmacc.vv     v24, v8, v0     \n\t" /* B[0, 2] * A[0 : 2, 0]     */
+                "vfmacc.vv     v25, v8, v1     \n\t" /* B[0, 2] * A[2 : 4, 0]     */
 
-                "vfmv.v.f   v11,  ft3          \n\t" // v11[i] = ft3                         // v11[i] = B[0, 3]
-                "vfmacc.vv  v24,  v10,    v0   \n\t" // v24[i] = (v10[i] * v0[i]) + v24[i]   // B[0, 2] * A[0 : 2, 0] + v24[i]
-                "vfmacc.vv  v25,  v10,    v1   \n\t" // v25[i] = (v10[i] * v1[i]) + v25[i]   // B[0, 2] * A[2 : 4, 0] + v25[i]
+                "fld        ft3,  (t3)         \n\t" /* ft3 = B[0, 3]             */
+                "addi       t3, t3, 4*8        \n\t" /* t3 = (B[1, 3])            */
+                "vfmv.v.f   v8,   ft3          \n\t" /* v8[i] = B[0, 3]           */
+                "vfmacc.vv     v28, v8, v0     \n\t" /* B[0, 3] * A[0 : 2, 0]     */
+                "vfmacc.vv     v29, v8, v1     \n\t" /* B[0, 3] * A[2 : 4, 0]     */
 
-                "vfmacc.vv  v28,  v11,    v0   \n\t" // v28[i] = (v11[i] * v0[i]) + v28[i]   // B[0, 3] * A[0 : 2, 0] + v28[i]
-                "vfmacc.vv  v29,  v11,    v1    \n\t" // v29[i] = (v11[i] * v1[i]) + v29[i]  // B[0, 3] * A[2 : 4, 0] + v29[i]
 
-                "addi       t0,   t0, -1        \n\t" // t0 = t0 + (-1)
-                "bgtz       t0,   M4x4_TAILLOOP \n\t" // If t0 > 0 goto M4x4_MAINLOOP
+                "addi       t0,   t0,     -1   \n\t" // t0 = t0 + (-1)
+                "bgtz       t0,   LOOP\n\t" // If t0 > 0 goto M4x4_TAILLOOP
+
 
                 // Save result
                 // load C
-                "M4x4_SAVERESULT:               \n\t"
                 // use v8 to store alpha
                 "vfmv.v.f   v8,   %[ALPHA]      \n\t" // v8[i] = %[ALPHA]     
                 "vle64.v      v0,   (%[C0])     \n\t" // v0[0 : 2] = (%[C0])    // v0[0 : 2] = C[0 : 2, 0]
@@ -96,10 +93,12 @@ void Gemm_MRxNRKernel_Packed( int k,
                 "vle64.v      v3,   (%[C3])     \n\t" // v3[0 : 2] = (%[C3])    // v1[0 : 2] = C[0 : 2, 3]
                 "addi       t3,   %[C3], 2*8    \n\t" // t3 = %[C3] + (2*8)     // t3 = (C[2, 3])
 
-                // Multiply Alpha
                 "vfmacc.vv  v0,   v8, v16       \n\t" // v0[i] = (v8[i] * v16[i]) + v0[i]  // C[0 : 2, 0] + (alpha * v16[0 : 2])
                 "vle64.v      v4,   (t4)        \n\t" // v4[0 : 2] = (t4)                  // v4[0 : 2] = (C[2 : 4, 0])
                 "vfmacc.vv  v1,   v8, v20       \n\t" // v1[i] = (v8[i] * v20[i]) + v1[i]  // C[0 : 2, 1] + (alpha * v20[0 : 2])
+                "vfmacc.vv  v4,   v8, v17       \n\t" // v4[i] = (v8[i] * v17[i]) + v4[i]  // C[2 : 4, 0] + (alpha * v17[0 : 2])
+                "vse64.v      v4,   (%[C0])     \n\t" // store updated C[0 : 2, 0] at (%[C0])
+
                 "vle64.v      v5,   (t5)        \n\t" // v5[0 : 2] = (t5)                  // v5[0 : 2] = (C[2 : 4, 1])
                 "vfmacc.vv  v2,   v8, v24       \n\t" // v2[i] = (v8[i] * v24[i]) + v2[i]  // C[0 : 2, 2] + (alpha * v24[0 : 2])
                 "vle64.v      v6,   (t6)        \n\t" // v6[0 : 2] = (t6)                  // v6[0 : 2] = (C[2 : 4, 2])
@@ -119,7 +118,11 @@ void Gemm_MRxNRKernel_Packed( int k,
                 "vfmacc.vv  v7,   v8, v29       \n\t" // v7[i] = (v8[i] * v29[i]) + v7[i]  // C[2 : 4, 3] + (alpha * v17[0 : 2])
                 "vse64.v     v3,   (%[C3])      \n\t" // store updated C[0 : 2, 1] at (%[C2])
                 "add        %[C3], %[C3], 4*8   \n\t" // %[C3] += (4*8)                    // (C[4, 3])
-
+                                                      //
+                "vse64.v      v4,   (t4)         \n\t" // store updated C[2 : 4, 0] at (t4)
+                "vse64.v      v5,   (t5)         \n\t" // store updated C[2 : 4, 1] at (t5) 
+                "vse64.v      v6,   (t6)         \n\t" // store updated C[2 : 4, 2] at (t6)
+                "vse64.v      v7,   (t3)         \n\t" // store updated C[2 : 4, 3] at (t6)
                 "M4x4_END:                      \n\t"
 
                 : [C0] "+r"(C0), [C1] "+r"(C1), [C2] "+r"(C2), [C3] "+r"(C3),
--- Makefile
+++ Makefile
@@ -1,2 +1,7 @@
+CFLAGS=-O3 -I$(BLAS_INC) -m64 -mavx2 -std=c99 -march=native -fopenmp -D_POSIX_C_SOURCE=200809L
+
 default: 
 	riscv64-unknown-elf-gcc main.c PackA.c PackB.c ./Gemm_4x4Kernel_RVV_inline.c -march=rv64gv -static -o gemm_rvv
+
+intel:
+	gcc PackA.c PackB.c Gemm_4x4Kernel_Packed_Intel.c main.c $(CFLAGS) -o gemm_intel
